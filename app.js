2fb916efa2b8cae90dea848fd1d189e258309b37
Oscar Rosete <oscaralonso11@hotmail.com>
2019-10-31 12:09:29 -0700

    changes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 // const port = 3000
+const hostname = '127.0.0.1';
 var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/public'));
