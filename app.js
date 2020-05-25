67791c1d653e7db247daa7625fd46229555ecb44
Oscar Rosete <oscaralonso11@hotmail.com>
2020-05-25 11:55:48 -0700

    coto mobile

--- app.js
+++ app.js
@@ -4,8 +4,25 @@ const hostname = '127.0.0.1';
 var port = process.env.PORT || 8080;
 /*https://www.npmjs.com/package/mysql*/
 var mysql      = require('mysql');
+const axios = require('axios');
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 
+app.use(express.json());       // to support JSON-encoded bodies
+app.use(express.urlencoded()); // to support URL-encoded bodies
 app.use(express.static(__dirname + '/public'));
+
+
+// const instance = axios.create({
+//     baseURL: 'http://localhost/',
+//     timeout: 1000,
+//     port:3000,
+//     headers: {'X-Custom-Header': 'foobar'}
+//   });
+
 app.get('/', (req, res) => {
     res.render ( "menu.ejs" );	
     // res.send('Hello World!'))
@@ -41,6 +58,50 @@ app.get('/login', (req, res) => {
     });	
     // res.send('Hello World!'))
 })
+
+app.post("/login",(req,res)=>{
+    var fullUrl = req.protocol + '://' + req.get('host') 
+    let requestUrl=`${fullUrl}/api/users`
+    // console.log(req.body)
+    console.log("=====requestUrl")
+    console.log(requestUrl)
+    // console.log(fullUrl)
+    axios.get(requestUrl)
+    .then(function (response) {
+      // handle success
+        let arr=response.data
+        var item = arr.findIndex(item => item.username === req.body.name);
+        console.log(item)
+        if(item==-1){
+            // res.json({
+            //     succes:"unsuccesful"
+            // })
+            res.redirect("/login")
+        }
+        else{
+            console.log(arr[item].passwords)
+            if(arr[item].passwords==req.body.pass){
+                // res.json({
+                //     succes:"succesful"
+                // })
+                res.redirect("/summary")
+            }
+            else{
+                // res.json({
+                //     succes:"unsuccesful"
+                // })
+                res.redirect("/login")
+            }
+        }
+    })
+    .catch(function (error) {
+      // handle error
+          console.log(error);
+        console.log("there was an error")
+        res.end()
+    })
+})
+
 app.get('/testinfo', (req, res) => {
     res.render ( "testinfo.ejs" ,{
         navbar:'navbar.ejs',
@@ -118,7 +179,7 @@ app.get("/api/:testparam",(req,res)=>{
     });
 })
 
-app.get("/api/:testparam",(req,res)=>{
+app.get("/api/users",(req,res)=>{
     var connection = mysql.createConnection({
         host     : 'coto1.cbsgdvm2kjtb.us-west-1.rds.amazonaws.com',
         post:'3306',
