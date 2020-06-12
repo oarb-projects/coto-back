7cb58f7144ee98d2a6c7c82c5e5402049b9ec950
Oscar Rosete <oscaralonso11@hotmail.com>
2020-06-12 11:50:39 -0700

    cors middleware

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+var cors = require('cors')
+
 require('dotenv').config()
 
 const hostname = '127.0.0.1';
@@ -10,6 +12,7 @@ const axios = require('axios');
 // body parser
 var bodyParser = require('body-parser')
 app.use( bodyParser.json() );       
+app.use(cors())
 
 app.use(express.json());       
 app.use(express.urlencoded()); 
