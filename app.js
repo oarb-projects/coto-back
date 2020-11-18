39d0c32752414225da3431ad76da2af60f0f0d48
Beto-cpu <62979859+Beto-cpu@users.noreply.github.com>
2020-11-18 14:17:47 -0800

    Add files via upload
    Rutas de graficas usando Chartjs agregadas. Estas paginas ya tienen el diseño y también son responsivas. Hace falta revisar el código para la generación de los datos de la gráfica ya que no corresponden a las anteriores gráficas.

--- app.js
+++ app.js
@@ -6,16 +6,16 @@ require('dotenv').config()
 
 const hostname = '127.0.0.1';
 var port = process.env.PORT || 8080;
-var mysql      = require('mysql');
+var mysql = require('mysql');
 const axios = require('axios');
 
 // body parser
 var bodyParser = require('body-parser')
-app.use( bodyParser.json() );       
+app.use(bodyParser.json());
 app.use(cors())
 
-app.use(express.json());       
-app.use(express.urlencoded()); 
+app.use(express.json());
+app.use(express.urlencoded());
 app.use(express.static(__dirname + '/public'));
 let objConnection = require('./config/db.config.js').objConnection;
 
@@ -23,119 +23,126 @@ var connection = mysql.createConnection(objConnection);
 console.log(objConnection)
 
 app.get('/', (req, res) => {
-    res.render ( "menu.ejs" );	
-    
+    res.render("menu.ejs");
+
 })
 
 app.get('/pareto', (req, res) => {
-    res.render ( "pareto.ejs",{
-        navbar:'navbar.ejs',
-        footer:'footer.ejs',
-        title:'Pareto Analysis'
-    } );	
+    res.render("pareto.ejs", {
+        navbar: 'navbar.ejs',
+        footer: 'footer.ejs',
+        title: 'Pareto Analysis'
+    });
 })
 
 app.get('/summary', (req, res) => {
-    res.render ( "summary.ejs",{
-        navbar:'navbar.ejs',
-        footer:'footer.ejs',
-        title:'Summary'
-    } );	
-    
+    res.render("summary.ejs", {
+        navbar: 'navbar.ejs',
+        footer: 'footer.ejs',
+        title: 'Summary'
+    });
+
 })
 
 app.get('/charts', (req, res) => {
-    res.render ( "charts.ejs",{
-        navbar:'navbar.ejs',
-        footer:'footer.ejs',
-        title:'Chart'
-    });	
-    
+    res.render("charts.ejs", {
+        navbar: 'navbar.ejs',
+        footer: 'footer.ejs',
+        title: 'Chart'
+    });
+
+})
+
+app.get('/chartsjs', (req, res) => {
+    res.render("chartsjs.ejs", {
+        navbar: 'navbar.ejs',
+        footer: 'footer.ejs',
+        chart_legend: 'chartjs-legend.ejs',
+        title: 'Chart'
+    });
 })
 
 app.get('/login', (req, res) => {
-    res.render ( "login.ejs",{
-        navbar:'navbar.ejs',
-        footer:'footer.ejs',
-        title:'Login'
-    });	
-    
+    res.render("login.ejs", {
+        navbar: 'navbar.ejs',
+        footer: 'footer.ejs',
+        title: 'Login'
+    });
+
 })
 
-app.post("/login",(req,res)=>{
-    var fullUrl = req.protocol + '://' + req.get('host') 
-    let requestUrl=`${fullUrl}/api/users`
+app.post("/login", (req, res) => {
+    var fullUrl = req.protocol + '://' + req.get('host')
+    let requestUrl = `${fullUrl}/api/users`
     console.log("=====requestUrl")
     console.log(requestUrl)
     axios.get(requestUrl)
-    .then(function (response) {
-        let arr=response.data
-        var item = arr.findIndex(item => item.username === req.body.name);
-        console.log(item)
-        if(item==-1){      
-            res.redirect("/login")
-        }
-        else{
-            console.log(arr[item].passwords)
-            if(arr[item].passwords==req.body.pass){
-                res.redirect("/filter")
-            }
-            else{
-
+        .then(function(response) {
+            let arr = response.data
+            var item = arr.findIndex(item => item.username === req.body.name);
+            console.log(item)
+            if (item == -1) {
                 res.redirect("/login")
+            } else {
+                console.log(arr[item].passwords)
+                if (arr[item].passwords == req.body.pass) {
+                    res.redirect("/filter")
+                } else {
+
+                    res.redirect("/login")
+                }
             }
-        }
-    })
-    .catch(function (error) {
-        console.log(error);
-        console.log("there was an error")
-        res.end()
-    })
+        })
+        .catch(function(error) {
+            console.log(error);
+            console.log("there was an error")
+            res.end()
+        })
 })
 
 app.get('/filter', (req, res) => {
-    res.render ( "filter.ejs",{
-        navbar:'navbar.ejs',
-        footer:'footer.ejs',
-        title:'File Selection Criteria'
-    });	
-    
+    res.render("filter.ejs", {
+        navbar: 'navbar.ejs',
+        footer: 'footer.ejs',
+        title: 'File Selection Criteria'
+    });
+
 })
 
 app.get('/testinfo', (req, res) => {
-    res.render ( "testinfo.ejs" ,{
-        navbar:'navbar.ejs',
-        footer:'footer.ejs',
-        title:'Test Information'
-    });	
-    
+    res.render("testinfo.ejs", {
+        navbar: 'navbar.ejs',
+        footer: 'footer.ejs',
+        title: 'Test Information'
+    });
+
 })
 
-app.get("/api/:testparam",(req,res)=>{
-    let param=req.params.testparam
-    console.log(param)    
-    connection.query(`SELECT * FROM Coto.${param};`, function (error, rows, fields) {
-        if (error){
-            console.log("Failed to query "+ error)
+app.get("/api/:testparam", (req, res) => {
+    let param = req.params.testparam
+    console.log(param)
+    connection.query(`SELECT * FROM Coto.${param};`, function(error, rows, fields) {
+        if (error) {
+            console.log("Failed to query " + error)
             res.status(404).render('dberr.ejs');
             return
-        } 
+        }
         res.json(rows)
     });
 })
 
-app.get("/api/users",(req,res)=>{
-      console.log(param)
-      connection.query(`SELECT * FROM Coto.users;`, function (error, rows, fields) {
-        if (error){
-            console.log("Failed to query "+ error)
-            res.status(404).render('dberr.ejs');    
-        } 
+app.get("/api/users", (req, res) => {
+    console.log(param)
+    connection.query(`SELECT * FROM Coto.users;`, function(error, rows, fields) {
+        if (error) {
+            console.log("Failed to query " + error)
+            res.status(404).render('dberr.ejs');
+        }
         res.json(rows)
     });
 })
 
-app.use(function(req,res){
+app.use(function(req, res) {
     res.status(404).render('404.ejs');
 });
 
---
+++ public/js/GraficasJs/RelT.js
@@ -0,0 +1,153 @@
+Chart.pluginService.register({
+    beforeDraw: function(chart, easing) {
+        if (chart.config.options.chartArea && chart.config.options.chartArea.backgroundColor) {
+            var ctx = chart.chart.ctx;
+            var chartArea = chart.chartArea;
+
+            ctx.save();
+            ctx.fillStyle = chart.config.options.chartArea.backgroundColor;
+            ctx.fillRect(chartArea.left, chartArea.top, chartArea.right - chartArea.left, chartArea.bottom - chartArea.top);
+            ctx.restore();
+        }
+    }
+});
+
+var urla = `https://coto-mobile.herokuapp.com/api/coil_resistance`;
+
+var api = new XMLHttpRequest();
+api.open('GET', urla, true);
+api.send();
+
+api.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        //console.log(this.responseText);
+        var data = JSON.parse(this.responseText);
+
+        var x = new Array(400).fill(0);
+        var labelsArray = [];
+        var colorArray = [];
+        var colorTextArray = [];
+
+        var limitA = 35;
+        var limitB = 45;
+
+        var total = [0];
+        var mean;
+
+        for (var i = limitA - 5; i <= limitB + 9; i += 0.5) {
+            if (i >= limitA && i <= limitB) {
+                colorArray.push('rgba(0, 0, 255, 1)');
+            } else {
+                colorArray.push('rgba(255, 0, 0, 1)');
+            }
+            labelsArray.push(`${i} - ${i+0.5}`);
+
+            colorTextArray.push('rgba(0,0,0, 1)');
+        }
+
+
+        for (let i of data) {
+            mean += Number(i.result1);
+
+            x[Math.round((i.result1 + 100) / 0.5)]++;
+        }
+
+
+        mean /= data.length;
+        document.getElementById('meanRT').innerHTML = `Mean: ${mean}`;
+
+        for (let i = 0; i < x.length; i++) {
+            x[i] *= 6;
+        }
+
+
+        var barChartData = {
+            type: 'bar',
+            data: {
+                labels: labelsArray,
+                datasets: [{
+                    label: "Frequency",
+                    backgroundColor: colorArray,
+                    borderColor: 'rgba(0,0,0,1)',
+                    borderWidth: 0.4,
+                    data: x.slice((limitA) * 2, (limitB + 100) * 2)
+                }]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true,
+                            min: 0,
+                            max: 60,
+                            fontColor: 'black'
+                        },
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Frequency'
+                        }
+                    }],
+                    xAxes: [{
+                        ticks: {
+                            fontColor: 'black'
+                        },
+                        categoryPercentage: 1.0,
+                        barPercentage: 1.0,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Usects',
+                        }
+                    }]
+                },
+                legend: {
+                    display: false,
+                    labels: {
+                        fontColor: 'black'
+                    }
+                },
+                chartArea: {
+                    backgroundColor: 'white'
+                },
+                annotation: {
+                    annotations: [{
+                        type: 'line',
+                        mode: 'vertical',
+                        scaleID: 'y-axis-0',
+                        value: -10,
+                        borderColor: 'rgb(75, 192, 192)',
+                        borderDash: [2, 2],
+                        borderWidth: 2,
+                        label: {
+                            enabled: true,
+                            position: "top",
+                            yAdjust: 20,
+                            content: 'Test limit'
+                        }
+                    }, {
+                        type: 'line',
+                        mode: 'vertical',
+                        scaleID: 'y-axis-0',
+                        value: 30,
+                        borderColor: 'rgb(75, 192, 192)',
+                        borderWidth: 2,
+                        yPadding: 6,
+                        borderDash: [2, 2],
+                        label: {
+                            enabled: true,
+                            position: "top",
+                            yAdjust: 20,
+                            content: 'Test limit'
+                        }
+                    }]
+                }
+            },
+
+        };
+
+        var ctx = document.getElementById("RT").getContext("2d");
+
+        var myChart = new Chart(ctx, barChartData);
+    }
+}
